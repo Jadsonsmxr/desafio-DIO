e741995c5235f3b3e784f154539dd0f83ab423fb
Jadson Maciel <jadsonmaciel@Jadsons-MacBook-Air.local>
2024-08-14 18:12:35 -0400

    first commit

---
+++ Desafio.playground/Contents.swift
@@ -0,0 +1,10 @@
+let nome = "Steve"
+var sobrenome: String? = "Wozniak"
+sobrenome = "Jobs"
+
+print("\(nome) \(sobrenome!)")
+
+if var sobrenome2 = sobrenome {
+    print("\(nome) \(sobrenome2)")
+}
+
